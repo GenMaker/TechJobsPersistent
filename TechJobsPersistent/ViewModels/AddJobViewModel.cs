03c5be123f1431815efa5c78ea4a17853fb2fdf1
genmakerlab@gmail.com <genmakerlab@gmail.com>
2021-01-04 20:51:21 -0600

    Finished Part 3

--- TechJobsPersistent/ViewModels/AddJobViewModel.cs
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using TechJobsPersistent.Models;
+using System.Collections.Generic;
+using TechJobsPersistent.Data;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
 namespace TechJobsPersistent.ViewModels
 {
     public class AddJobViewModel
     {
+        
+  
+        public int EmployerId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(12, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        public List<Skill> Skills { get; set; }
+
+        public List<SelectListItem> Employers { get; set; }
+   
+
         public AddJobViewModel()
         {
+
         }
+
+        public AddJobViewModel ( List<Employer> employers, List<Skill> skills)
+        {
+            Employers = new List<SelectListItem>();
+            Skills = skills;
+
+            foreach (Employer employer in employers)
+            {
+                Employers.Add(new SelectListItem {
+                    Text= employer.Name,
+                    Value= employer.Id.ToString()
+                    }
+                );
+            }
+        }
+
     }
 }
